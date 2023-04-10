8326610aa650de83819a615dd446c227798f9ac5
Rahat ( umm-arwa) <124298703+Rahat25252@users.noreply.github.com>
2023-04-10 22:58:35 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,49 @@
+// ---------------Question 1-----------------
+// var username = "Rahat";
+// console.log(username);
+
+
+// ---------------Question 2----------------- 
+//     var myName = "Rahat umer";
+//  console.log(myName);
+
+// ---------------Question 3-----------------
+    // var message = "Hello World"
+    // alert (message);
+
+
+// ---------------Question 4-----------------
+    // var studentName = "Jhone Doe";
+    // alert (studentName);
+
+    // var studentAge = "15 years old";
+    // alert (studentAge);
+
+    // var studentSkill = "Certified Mobile Application Development";
+    // alert (studentSkill);
+
+
+// ---------------Question 5-----------------
+    // var name = " \n PIZZA \n PIZZ \n PIZ \n PI \n P";
+    // alert (name);
+
+
+// ---------------Question 6-----------------
+    // var email = "example@example.com";
+    // alert ( "My email address is " + email);
+        
+
+// ---------------Question 7-----------------
+    // var book = "A smarter way to learn JavaScript";
+    // alert ("I am trying to learn from the book " + book);
+
+
+// ---------------Question 8-----------------
+// var content="Yah I can write HTML content through JavaScript"
+//     document.write("Yah I can write HTML content through JavaScript")
+
+
+// ---------------Question 9-----------------
+
+// var string = "▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬";
+// alert (string);
